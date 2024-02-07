d4f701cd83f68e05beb699353e52228606b6c255
Berke Asil <berkeasil2004@gmail.com>
2024-02-07 16:05:56 +0300

    All problems are fixed

--- Homework.java
+++ Homework.java
@@ -2,8 +2,18 @@ import java.util.*;
 class Homework{
 
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
+
+        int arraySize;
         int choice;
+
+        System.out.println("Enter the size of the array");
+
+        arraySize = in.nextInt();
+
+        int [] mainArray = createRandomArray(arraySize);
+        System.out.println("Your array is " + Arrays.toString(mainArray));
         do{
             System.out.println("1. Find the minimum of the array");
             System.out.println("2. Find the maximum of the array");
@@ -16,22 +26,22 @@ class Homework{
             if(choice == 1)
             {
                 System.out.println("The minimum of the array: ");
-                findMin();
+                findMin(mainArray);
             }
             if(choice == 2)
             {
                 System.out.println("The maximum of the array: ");
-                findMax();
+                findMax(mainArray);
             }
             if(choice == 3)
             {
                 System.out.println("The average and the differences between numbers and average: ");
-                findAverage();
+                findAverage(mainArray);
             }
             if(choice == 4)
             {
                 System.out.println("Sum of even indexed numbers and sum of odd indexed numbers: ");
-                oddEven();
+                oddEven(mainArray);
             }
             if(choice == 5)
             {
@@ -52,6 +62,49 @@ class Homework{
         }
         return arr;
     }
+
+    public static void findMin(int [] numbers){
+
+        int minNum = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] < minNum){
+                minNum = numbers[i];
+            }
+        }
+        System.out.println(minNum);
+    }
+
+    public static void findMax(int [] numbers){
+
+        int maxNum = numbers[0];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] > maxNum){
+                maxNum = numbers[i];
+            }
+        }
+        System.out.println(maxNum);
+    }
+
+    public static void findAverage(int [] numbers){
+        int sum = 0;
+
+        for(int value : numbers){
+            sum += value;
+        }
+        
+        int average = sum / numbers.length;
+
+        int [] copiedNums = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            copiedNums[i] = numbers[i] - average;
+        }
+        System.out.println("The average is " + average);
+        System.out.println(Arrays.toString(copiedNums));
+    }
+
     public static void oddEven(int[] arr) {
         int odd = 0;
         int even = 0;
@@ -67,51 +120,4 @@ class Homework{
 
         System.out.println(even +", " + odd);
      }
-
-import java.util.Arrays;
-
-public class Homework{
-
-    public int findMin(int [] numbers){
-
-        int minNum = numbers[0];
-
-        for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] < minNum){
-                minNum = numbers[i];
-            }
-        }
-        return minNum;
-    }
-
-    public int findMax(int [] numbers){
-
-        int maxNum = numbers[0];
-        
-        for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] > maxNum){
-                maxNum = numbers[i];
-            }
-        }
-        return maxNum;
-    }
-
-    public int findAverage(int [] numbers){
-        int sum = 0;
-
-        for(int value : numbers){
-            sum += value;
-        }
-        
-        int average = sum / numbers.length;
-
-        int [] copiedNums = new int[numbers.length];
-
-        for (int i = 0; i < numbers.length; i++) {
-            copiedNums[i] = numbers[i] - average;
-        }
-        System.out.println(Arrays.toString(copiedNums));
-
-        return average;
-    }
 }
